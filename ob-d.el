799062a00d2718f49790e8755b21e7819a32b78e
jordanschupbach <jordans1882@gmail.com>
2026-06-05 08:38:24 -0600

    Cleanup and move ob-d

--- ob-d.el
+++ ob-d.el
@@ -1,150 +1,77 @@
-;;; ob-d.el --- Org-Babel support for D -*- lexical-binding: t; -*-
 
-(require 'org-macs)
-(org-assert-version)
+;;; ob-d.el --- Babel Functions for D -*- lexical-binding: t; -*-
 
 (require 'ob)
-(require 'ob-eval)
+(require 'org-macs)
 
-(defvar org-babel-tangle-lang-exts)
+(defgroup org-babel-d nil
+  "Org Babel support for D."
+  :group 'org-babel)
+
+(defcustom org-babel-d-compiler "ldc2"
+  "Command used to compile D source code."
+  :group 'org-babel-d
+  :type 'string)
+
+(defvar org-babel-default-header-args:d '())
+
 (add-to-list 'org-babel-tangle-lang-exts '("d" . "d"))
 
-(defvar org-babel-default-header-args:d
-  '((:results . "output")
-    (:exports . "results"))
-  "Default header arguments for D source blocks.")
-
-(defcustom org-babel-d-command "dmd"
-  "Name of the command to use for executing D code."
-  :group 'org-babel
-  :type 'string)
-
-(defcustom org-babel-d-hline-to "null"
-  "Replace hlines in incoming tables with this when translating to D."
-  :group 'org-babel
-  :type 'string)
-
-(defcustom org-babel-d-nil-to 'hline
-  "Replace nil in D tables with this before returning."
-  :group 'org-babel
-  :type 'symbol)
-
 (defun org-babel-execute:d (body params)
-  "Execute D BODY according to PARAMS.
-This function is called by `org-babel-execute-src-block'."
-  (let* ((result-params (cdr (assq :result-params params)))
-         (result-type (cdr (assq :result-type params)))
-         (org-babel-d-command (or (cdr (assq :d params)) org-babel-d-command))
-         (full-body (org-babel-expand-body:generic
-                     body params (org-babel-variable-assignments:d params)))
-         (results (org-babel-d-evaluate full-body result-type result-params)))
-    (org-babel-reassemble-table
-     (org-babel-result-cond result-params
-       results
-       (org-babel-d-table-or-string results))
-     (org-babel-pick-name (cdr (assq :colname-names params))
-                          (cdr (assq :colnames params)))
-     (org-babel-pick-name (cdr (assq :rowname-names params))
-                          (cdr (assq :rownames params))))))
+  "Execute D BODY according to PARAMS."
+  (let* ((src-file (org-babel-temp-file "ob_d_" ".d"))
+         (bin-file (org-babel-temp-file "ob_d_bin_" org-babel-exeext))
+         (cmdline (cdr (assq :cmdline params)))
+         (flags (cdr (assq :flags params)))
+         (full-body (org-babel-expand-body:d body params src-file)))
+    (with-temp-file src-file
+      (insert full-body))
+    (org-babel-eval
+     (format "%s -of=%s %s %s"
+             org-babel-d-compiler
+             (org-babel-process-file-name bin-file)
+             (mapconcat #'identity
+                        (delq nil (if (listp flags) flags (list flags)))
+                        " ")
+             (org-babel-process-file-name src-file))
+     "")
+    (org-babel-eval
+     (concat
+      (org-babel-process-file-name bin-file)
+      (if cmdline (concat " " cmdline) ""))
+     "")))
 
-(defun org-babel-prep-session:d (_session _params)
-  "Prepare a D session.
-
-Org Babel D blocks currently do not support sessions."
-  (error "Org Babel D does not support sessions"))
-
-(defun org-babel-variable-assignments:d (params)
-  "Return a list of D statements assigning variables from PARAMS."
-  (mapcar
-   (lambda (pair)
-     (format "auto %s = %s;" (car pair) (org-babel-d-var-to-d (cdr pair))))
-   (org-babel--get-vars params)))
-
-(defun org-babel-d-var-to-d (var)
-  "Convert VAR into a string of D source code representing VAR."
-  (cond
-   ((eq var 'hline) org-babel-d-hline-to)
-   ((null var) "null")
-   ((numberp var) (number-to-string var))
-   ((eq var t) "true")
-   ((stringp var) (org-babel-d--quote-string var))
-   ((and (listp var) (org-babel-d--alist-p var))
-    (concat "["
-            (mapconcat
-             (lambda (cell)
-               (format "%s: %s"
-                       (org-babel-d-var-to-d (car cell))
-                       (org-babel-d-var-to-d (cdr cell))))
-             var ", ")
-            "]"))
-   ((listp var)
-    (concat "["
-            (mapconcat #'org-babel-d-var-to-d var ", ")
-            "]"))
-   (t (org-babel-d--quote-string (format "%s" var)))))
-
-(defun org-babel-d--alist-p (value)
-  (and (listp value)
-       (catch 'not-alist
-         (dolist (el value t)
-           (unless (consp el) (throw 'not-alist nil))))))
-
-(defun org-babel-d--quote-string (s)
-  (concat "\""
-          (replace-regexp-in-string
-           "[\"\\\\\n\r\t]"
-           (lambda (m)
-             (pcase m
-               ("\n" "\\n")
-               ("\r" "\\r")
-               ("\t" "\\t")
-               (_ (concat "\\" m))))
-           s
-           t t)
-          "\""))
-
-(defun org-babel-d-evaluate (body result-type _result-params)
-  "Evaluate D BODY and return results according to RESULT-TYPE."
-  (let* ((tmp-file (org-babel-temp-file "d-" ".d"))
-         (wrapper (org-babel-d--wrap body result-type))
-         (command (format "%s -run %s"
-                          org-babel-d-command
-                          (org-babel-process-file-name tmp-file))))
-    (with-temp-file tmp-file (insert wrapper))
-    (org-babel-eval command "")))
-
-(defun org-babel-d--wrap (body result-type)
-  (let ((value? (eq result-type 'value)))
-    (concat
-     "import std.stdio;\n"
-     "\n"
-     "void main() {\n"
-     (org-babel-d--indent (org-babel-chomp body) 2)
-     "\n"
-     (when value?
-       (concat
-        "\n"
-        "  static if (__traits(compiles, result)) {\n"
-        "    write(result);\n"
-        "  }\n"))
-     "}\n")))
-
-(defun org-babel-d--indent (body spaces)
-  "Indent each line of BODY by SPACES."
-  (let ((prefix (make-string spaces ?\s)))
+(defun org-babel-expand-body:d (body params &optional src-file)
+  "Expand D BODY according to PARAMS.
+When SRC-FILE is non-nil, derive the module name from it."
+  (let* ((imports (cdr (assq :imports params)))
+         (imports (if (stringp imports) (split-string imports) imports))
+         (module-name (org-babel-d--module-name src-file)))
     (mapconcat
-     (lambda (line) (concat prefix line))
-     (split-string body "\n" nil)
-     "\n")))
+     #'identity
+     (delq
+      nil
+      (list
+       (format "module %s;" module-name)
+       (mapconcat (lambda (imp) (format "import %s;" imp))
+                  (delete-dups (append imports '("std.stdio")))
+                  "\n")
+       (org-babel-d--ensure-main body)))
+     "\n\n")))
 
-(defun org-babel-d-table-or-string (results)
-  "Convert RESULTS into an appropriate elisp value.
-If RESULTS look like an Org table, return it as a table. Otherwise
-return a string."
-  (let ((res (org-babel-script-escape results)))
-    (if (listp res)
-        (mapcar (lambda (el) (if (not el) org-babel-d-nil-to el)) res)
-      res)))
+(defun org-babel-d--module-name (&optional src-file)
+  "Return a valid D module name for SRC-FILE."
+  (let* ((base (file-name-base (or src-file buffer-file-name "ob_d_tmp")))
+         (name (replace-regexp-in-string "[^[:alnum:]_]" "_" base)))
+    (if (string-match-p "\\`[[:digit:]]" name)
+        (concat "_" name)
+      name)))
+
+(defun org-babel-d--ensure-main (body)
+  "Wrap BODY in `main' if needed."
+  (if (string-match-p "^[[:space:]\n\r]*\\(void\\|int\\)[[:space:]\n\r]+main[[:space:]]*(" body)
+      body
+    (format "void main() {\n%s\n}" body)))
 
 (provide 'ob-d)
 
